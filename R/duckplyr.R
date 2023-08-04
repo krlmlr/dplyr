05182bec0e693aedbc8c7b00220de2bce3d5171c
Kirill Müller <kirill@cynkra.com>
2023-08-04 23:05:16 +0200

    Ouch

--- R/duckplyr.R
+++ R/duckplyr.R
@@ -3,3 +3,15 @@ dplyr_mode <- TRUE
 on_load({
   options(duckdb.materialize_message = FALSE)
 })
+
+meta_rel_register <- function(...) {
+}
+
+meta_rel_register_df <- function(...) {
+}
+
+meta_rel_register_csv <- function(...) {
+}
+
+meta_macro_register <- function(...) {
+}
