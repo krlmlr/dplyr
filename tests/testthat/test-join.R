408b9610cbda6e06da0fcc6fcc7d9e26bae22530
Kirill Müller <kirill@cynkra.com>
2024-04-25 08:10:04 +0200

    Sync with duckdblabs/duckplyr@235299c5bc98db5d678757c32cdae8db59131034

--- tests/testthat/test-join.R
+++ tests/testthat/test-join.R
@@ -382,7 +382,6 @@ test_that("mutating joins compute common columns", {
 })
 
 test_that("filtering joins compute common columns", {
-  skip("TODO duckdb")
   df1 <- tibble(x = c(1, 2), y = c(2, 3))
   df2 <- tibble(x = c(1, 3), z = c(2, 3))
   expect_snapshot(out <- semi_join(df1, df2))
